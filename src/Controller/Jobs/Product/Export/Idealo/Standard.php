90e6007e79b8248415112ffc5c5181da1c3fd79b
Aimeos <aimeos@aimeos.org>
2026-04-07 13:31:37 +0200

    Don't exclude selection products if a variant has an excluded attribute

--- src/Controller/Jobs/Product/Export/Idealo/Standard.php
+++ src/Controller/Jobs/Product/Export/Idealo/Standard.php
@@ -228,6 +228,7 @@ class Standard
 		$filter = $this->filter( $manager->filter( true ), $feedItem );
 		$excludeCats = $feedItem->getListItems( 'catalog', 'exclude' )->getRefId();
 		$excludeSupps = $feedItem->getListItems( 'supplier', 'exclude' )->getRefId();
+		$attrExclIds = array_filter( array_column( $feedItem->getConfig()['attribute_excludes'] ?? [], 'id' ) );
 
 		$cursor = $manager->cursor( $filter );
 		$domains = $this->domains();
@@ -246,6 +247,26 @@ class Standard
 			{
 				$items = $items->filter( fn( $item ) => $item->getListItems( 'catalog' )->getRefId()->intersect( $excludeCats )->isEmpty() );
 				$items = $items->filter( fn( $item ) => $item->getListItems( 'supplier' )->getRefId()->intersect( $excludeSupps )->isEmpty() );
+
+				if( !empty( $attrExclIds ) )
+				{
+					foreach( $items as $product )
+					{
+						if( $product->getType() !== 'select' ) {
+							continue;
+						}
+
+						foreach( $product->getListItems( 'product', 'default' ) as $listItem )
+						{
+							$variant = $listItem->getRefItem();
+
+							if( $variant && array_intersect( $variant->getRefItems( 'attribute' )->keys()->all(), $attrExclIds ) ) {
+								$product->deleteListItem( 'product', $listItem, $variant );
+							}
+						}
+					}
+				}
+
 				$items = $this->call( 'hydrate', $items );
 
 				if( fwrite( $fh, $this->render( $items, $feedItem ) ) === false ) {
@@ -328,7 +349,10 @@ class Standard
 		$attrExclIds = array_filter( array_column( $item->getConfig()['attribute_excludes'] ?? [], 'id' ) );
 
 		if( !empty( $attrExclIds ) ) {
-			$excludes[] = $filter->is( 'index.attribute.id', '!=', $attrExclIds );
+			$excludes[] = $filter->or( [
+				$filter->is( 'product.type', '==', 'select' ),
+				$filter->is( 'index.attribute.id', '!=', $attrExclIds ),
+			] );
 		}
 
 		return $filter->add( $filter->and( [
